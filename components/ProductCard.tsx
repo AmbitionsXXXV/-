3bd09feeb8cc7408249b9dc5b5dcca3ccbe2eb32
Etcetera <yilonglei790@gmail.com>
2023-02-13 11:17:52 +0800

    简单页面展示

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,18 +1,20 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 
 export default function ProductCard(props: any) {
   const [isLoading, setLoading] = useState(true)
   const { product } = props
 
   return (
-    <Link href={`/products/${product.id}`} className="group">
+    // <Link href={`/products/${product.id}`} className="group">
+    <div className="group">
       <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt="product image"
-          src={product.imageGroups[0].images[0].link}
-          fill
+          src={product.imageGroups.image.link}
+          width={800}
+          height={70}
           className={
             'object-cover duration-700 ease-in-out group-hover:opacity-75'
           }
@@ -25,6 +27,7 @@ export default function ProductCard(props: any) {
       <p className="mt-1 text-sm italic text-gray-500">
         {product.shortDescription}
       </p>
-    </Link>
+    </div>
+    // </Link>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,46 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Header from '@/components/Header'
 import ProductCard from '@/components/ProductCard'
 
 const Home: NextPage = () => {
-  const data: any[] = []
+  const data: any[] = [
+    {
+      imageGroups: { image: { link: '/login.png' } },
+      name: '登陆注册页面',
+      shortDescription: '功能：用户进入主页前的登陆注册',
+      id: 1
+    },
+    {
+      imageGroups: { image: { link: '/addUser.png' } },
+      name: '用户管理页面',
+      shortDescription: '功能：负责操作管理用户',
+      id: 2
+    },
+    {
+      imageGroups: { image: { link: '/itemBank.png' } },
+      name: '题库管理页面',
+      shortDescription: '功能：负责操作管理题库',
+      id: 3
+    },
+    {
+      imageGroups: { image: { link: '/addPaper.png' } },
+      name: '考试试卷管理页面',
+      shortDescription: '功能：操作管理试卷页面',
+      id: 4
+    },
+    {
+      imageGroups: { image: { link: '/paperList.png' } },
+      name: '试卷题目页面',
+      shortDescription: '功能：列表形式展示所有试卷',
+      id: 5
+    },
+    {
+      imageGroups: { image: { link: '/users.png' } },
+      name: '用户列表页面',
+      shortDescription: '功能：展示所有用户及所拥有的权限',
+      id: 6
+    }
+  ]
 
   return (
     <>
@@ -18,11 +53,10 @@ const Home: NextPage = () => {
             </p>
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {data &&
-            data.map((product) => (
-              <ProductCard product={product} key={product.id} />
-            ))}
+        <div className="mb-3 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
+          {data.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
         </div>
       </div>
     </>
