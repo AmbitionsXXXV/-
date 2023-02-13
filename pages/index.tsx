642af184531971f7ec4043bbdda5ed3b1be8fabb
Etcetera <yilonglei790@gmail.com>
2023-02-13 15:49:47 +0800

    footer组件

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,11 @@
 import type { NextPage } from 'next'
 import Header from '@/components/Header'
 import ProductCard from '@/components/ProductCard'
+import { imgData } from 'data'
+import Footer from '@/components/Footer'
 
 const Home: NextPage = () => {
-  const data: any[] = [
-    {
-      imageGroups: { image: { link: '/login.png' } },
-      name: '登陆注册页面',
-      shortDescription: '功能：用户进入主页前的登陆注册',
-      id: 1
-    },
-    {
-      imageGroups: { image: { link: '/addUser.png' } },
-      name: '用户管理页面',
-      shortDescription: '功能：负责操作管理用户',
-      id: 2
-    },
-    {
-      imageGroups: { image: { link: '/itemBank.png' } },
-      name: '题库管理页面',
-      shortDescription: '功能：负责操作管理题库',
-      id: 3
-    },
-    {
-      imageGroups: { image: { link: '/addPaper.png' } },
-      name: '考试试卷管理页面',
-      shortDescription: '功能：操作管理试卷页面',
-      id: 4
-    },
-    {
-      imageGroups: { image: { link: '/paperList.png' } },
-      name: '试卷题目页面',
-      shortDescription: '功能：列表形式展示所有试卷',
-      id: 5
-    },
-    {
-      imageGroups: { image: { link: '/users.png' } },
-      name: '用户列表页面',
-      shortDescription: '功能：展示所有用户及所拥有的权限',
-      id: 6
-    }
-  ]
+  const data = imgData
 
   return (
     <>
@@ -59,6 +24,7 @@ const Home: NextPage = () => {
           ))}
         </div>
       </div>
+      <Footer />
     </>
   )
 }
